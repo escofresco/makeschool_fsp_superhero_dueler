26619a5580f03e6982c8592d238e6dc3a49c7542
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-29 16:43:07 -0700

    Add basic functionality

---
+++ superheroes.py
@@ -0,0 +1,61 @@
+from random import randrange, shuffle
+
+class Ability(object):
+    def __init__(self, name: str, max_damage: int):
+        self.name = name
+        self.max_damage
+
+    def attack(self) -> int:
+        return randrange(self.max_damage+1)
+
+class Armor(object):
+    def __init__(self, name: str, max_block: int):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        return randrange(self.max_block+1)
+
+class Weapon(Ability):
+    def attack(self):
+        return randrange(self.max_damage//2, self.max_damage)
+
+class Hero(object):
+    def __init__(self, name: str, starting_health=100):
+        self.name = name
+        self.starting_health = self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
+
+    def add_ability(self, ability: Ability) -> None:
+        self.abilities.append(ability)
+
+    def add_armor(self, armor: Armor) -> None:
+        self.armors.append(armor)
+
+    def attack(self) -> int:
+        return sum(ability.attack() for ability in self.abilities)
+
+    def defend(self, damage: int) -> int:
+        return damage - sum(armor.block() for armor in self.armors)
+
+    def take_damage(self, damage: int) -> None:
+        self.current_health -= self.defend(damage)
+
+    def is_alive(self) -> bool:
+        return self.current_health > 0
+
+    def fight(self, opponent: Hero) -> None:
+        if len(self.abilities) == len(opponent.abilities) == 0:
+            print("Draw")
+            return
+        # guarantee that the first attack is random
+        player1, player2 = shuffle([self, opponent])
+        while player1.is_alive() and player2.is_alive():
+            player2.take_damage(player1.attack())
+            if not (player1.is_alive() and player2.is_alive()):
+                break
+            player1.take_damage(player2.attack())
+        print(f"{
+              player1.name if player1.is_alive() else player2.name
+        } won")
