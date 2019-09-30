1b4e703ae667ad14d5b14ca37fb021650bfdcfab
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-30 10:54:55 -0700

    fix missing assignment and type hint issues

--- superheroes.py
+++ superheroes.py
@@ -3,7 +3,7 @@ from random import randrange, shuffle
 class Ability(object):
     def __init__(self, name: str, max_damage: int):
         self.name = name
-        self.max_damage
+        self.max_damage = max_damage
 
     def attack(self) -> int:
         return randrange(self.max_damage+1)
@@ -20,34 +20,24 @@ class Weapon(Ability):
     def attack(self):
         return randrange(self.max_damage//2, self.max_damage)
 
-class Team(object):
-    def __init__(self, name: str):
-        self.name = name
-
-        # order doesn't matter so let's use a data structure
-        # with constant del/add
-        self.members = {}
-
-    def add_hero(self, hero: Hero):
-        self.members[hero.name] = hero
-
-    def remove_hero(self, name: str):
-        if name in self.members:
-            del self.members[name]
-
-    def view_all(self):
-        for hero in self.members:
-            print(hero)
-
 class Hero(object):
     def __init__(self, name: str, starting_health=100):
         self.name = name
-        self.starting_health = self.current_health = starting_health
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
         self.abilities = []
         self.armors = []
 
     def __str__(self):
         return self.name
+
+    def add_kill(self):
+        self.kills += 1
+
+    def add_deaths(self):
+        self.deaths += 1
 
     def add_ability(self, ability: Ability) -> None:
         self.abilities.append(ability)
@@ -67,7 +57,7 @@ class Hero(object):
     def is_alive(self) -> bool:
         return self.current_health > 0
 
-    def fight(self, opponent: Hero) -> None:
+    def fight(self, opponent: 'Hero') -> None:
         if len(self.abilities) == len(opponent.abilities) == 0:
             print("Draw")
             return
@@ -78,6 +68,43 @@ class Hero(object):
             if not (player1.is_alive() and player2.is_alive()):
                 break
             player1.take_damage(player2.attack())
-        print(f"{
-              player1.name if player1.is_alive() else player2.name
-        } won")
+        player1.kills += player1.is_alive() ^ 0
+        player1.deaths += player1.is_alive() ^ 1
+        player2.kills += player2.is_alive() ^ 0
+        player2.deaths += player2.is_alive() ^ 1
+        print(f"{(player1.name if player1.is_alive() else player2.name)} won")
+
+class Team(object):
+    def __init__(self, name: str):
+        self.name = name
+
+        # order doesn't matter so let's use a data structure
+        # with constant del/add
+        self.heroes = []
+
+    def add_hero(self, hero: Hero):
+        #self.heroes[hero.name] = hero
+        self.heroes.append(hero)
+
+    def remove_hero(self, name: str):
+        # if name not in self.members:
+        #     return 0
+        # del self.members[name]
+        for i, hero in enumerate(self.heroes):
+            if hero.name == name:
+                del self.heroes[i]
+                return
+        return 0
+
+    def view_all_heroes(self):
+        for hero in self.heroes:
+            print(hero)
+
+    def attack(self, other_team: 'Team'):
+        pass
+
+    def revive_heroes(self, health=100):
+        pass
+
+    def stats(self):
+        pass
